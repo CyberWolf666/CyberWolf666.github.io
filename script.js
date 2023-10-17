b54136f3cbb7bbf473acd9d12cc05685ad7b8db8
CyberWolf666 <148146152+CyberWolf666@users.noreply.github.com>
2023-10-17 10:02:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/* Credit and Thanks:
-Matrix - Particles.js;
-SliderJS - Ettrics;
-Design - Sara Mazal Web;
-Fonts - Google Fonts
-*/
-
 window.onload = function () {
   Particles.init({
     selector: ".background"
@@ -133,9 +126,3 @@ class NavigationPage {
 }
 
 new NavigationPage();
-/* Credit and Thanks:
-Matrix - Particles.js;
-SliderJS - Ettrics;
-Design - Sara Mazal Web;
-Fonts - Google Fonts
-*/
